7d54dc0b20426964a6612799fed9d921f0d247e6
jmirzaev <jmirzaev@texnomart.uz>
2022-09-12 17:55:48 +0500

    remove check network from repositories

--- app/src/main/java/com/example/movieapp/repository/GenreRepository.kt
+++ app/src/main/java/com/example/movieapp/repository/GenreRepository.kt
@@ -1,11 +1,9 @@
 package com.example.movieapp.repository
 
 import android.app.Application
-import android.content.Context
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
 import com.example.movieapp.model.Genre
 import com.example.movieapp.network.api.GenreApi
+import com.example.movieapp.utils.Utils
 import javax.inject.Inject
 
 class GenreRepository @Inject constructor(
@@ -14,28 +12,14 @@ class GenreRepository @Inject constructor(
 ) {
 
     suspend fun getGenreMovieList(): List<Genre> {
-        return if (networkAvailable(application))
+        return if (Utils.networkAvailable(application))
             genreApi.getGenreMovieList().genres
         else emptyList()
     }
 
     suspend fun getGenreTVList(): List<Genre> {
-        return if (networkAvailable(application))
+        return if (Utils.networkAvailable(application))
             genreApi.getGenreTVList().genres
         else emptyList()
-    }
-
-    private fun networkAvailable(context: Context): Boolean {
-        val connectivityManager =
-            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val networkCapabilities = connectivityManager.activeNetwork ?: return false
-        val actNw =
-            connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
-        return when {
-            actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-            actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-            actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
-            else -> false
-        }
     }
 }
--- app/src/main/java/com/example/movieapp/repository/MovieRepository.kt
+++ app/src/main/java/com/example/movieapp/repository/MovieRepository.kt
@@ -1,13 +1,11 @@
 package com.example.movieapp.repository
 
 import android.app.Application
-import android.content.Context
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
 import com.example.movieapp.model.Cast
 import com.example.movieapp.model.Movie
 import com.example.movieapp.network.api.CastApi
 import com.example.movieapp.network.api.MovieApi
+import com.example.movieapp.utils.Utils
 import javax.inject.Inject
 
 class MovieRepository @Inject constructor(
@@ -17,33 +15,19 @@ class MovieRepository @Inject constructor(
 ) {
 
     suspend fun getPopularMovieList(genreId: Int?): List<Movie> {
-        return if (networkAvailable(application))
+        return if (Utils.networkAvailable(application))
             movieApi.getPopularMovieList(genreId).results
         else emptyList()
     }
 
     suspend fun getMovieById(movieId: Long): Movie? {
-        return if (networkAvailable(application))
+        return if (Utils.networkAvailable(application))
             movieApi.getMovieById(movieId)
         else null
     }
 
-    private fun networkAvailable(context: Context): Boolean {
-        val connectivityManager =
-            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val networkCapabilities = connectivityManager.activeNetwork ?: return false
-        val actNw =
-            connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
-        return when {
-            actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-            actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-            actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
-            else -> false
-        }
-    }
-
     suspend fun getCastOfMovie(movieId: Long): List<Cast> {
-        return if (networkAvailable(application))
+        return if (Utils.networkAvailable(application))
             castApi.getCastListOfMovie(movieId).cast
         else emptyList()
     }
