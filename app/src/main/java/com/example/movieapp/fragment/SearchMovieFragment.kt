96fd1d6e5f32ad090c2820c0dfee83d8d3539f76
jmirzaev <jmirzaev@texnomart.uz>
2022-09-19 16:23:37 +0500

    update fragment_search.xml

--- app/src/main/java/com/example/movieapp/fragment/SearchMovieFragment.kt
+++ app/src/main/java/com/example/movieapp/fragment/SearchMovieFragment.kt
@@ -1,15 +1,8 @@
 package com.example.movieapp.fragment
 
-import android.content.Context
 import android.os.Bundle
-import android.os.ResultReceiver
 import android.view.View
-import android.view.inputmethod.InputMethodManager
-import androidx.core.content.ContextCompat.getSystemService
 import com.example.movieapp.databinding.FragmentSearchBinding
-import java.lang.reflect.InvocationTargetException
-import java.lang.reflect.Method
-
 
 class SearchMovieFragment : BaseFragment<FragmentSearchBinding>(FragmentSearchBinding::inflate) {
 
